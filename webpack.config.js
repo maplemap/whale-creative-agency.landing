884612501ba49288c227e9945a1a159633218168
Sergey.Illarionov <easthunch@gmail.com>
2017-02-09 11:03:12 +0200

    update: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,104 +1,198 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const mixins = require('postcss-mixins');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = nodeEnv === 'production';
 const NODE_HOST = process.env.NODE_HOST || '0.0.0.0';
 const NODE_PORT = process.env.NODE_PORT || 8090;
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+function getEntrySources(sources) {
+  if (!isProd) {
+    sources.unshift('react-hot-loader/patch');
+    sources.unshift(`webpack-dev-server/client?http://${NODE_HOST}:${NODE_PORT}`);
+    sources.unshift('webpack/hot/dev-server');
+  }
 
-module.exports = {
-    entry: getEntrySources(['./src/main.js']),
-    output: {
-        path: __dirname + '/public/build',
-        publicPath: "/build/",
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: (process.env.NODE_ENV === "production")
-                            ? ExtractTextPlugin.extract('style', 'css!autoprefixer')
-                            : "style-loader!css-loader!autoprefixer-loader",
-                exclude: [/node_modules/, /public/]
-            },
-            {
-                test: /\.less$/,
-                loader: (process.env.NODE_ENV === "production")
-                            ? ExtractTextPlugin.extract('style', 'css!less!autoprefixer')
-                            : "style-loader!css-loader!autoprefixer-loader!less",
-                exclude: [/node_modules/, /public/]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg|ico)$/i,
-                loaders: [
-                    'file?name=[sha512:hash:base64:7].[ext]',
-                    'image-webpack?progressive=true&optimizationLevel=7&interlaced=true'
-                ],
-                exclude: [/node_modules/, /public/, /src\/fonts/]
-            },
-            {
-                test   : /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-                loader : 'file?name=[name].[ext]',
-                exclude: [/node_modules/, /public/, /src\/img/]
-            },
-            {
-                test: /\.json$/,
-                loader: "json-loader"
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ["es2015", "stage-0", "react"],
-                    plugins: ["react-hot-loader/babel"]
-                }
-            }
-        ]
-    },
-
-    devServer: {
-        colors: true,
-        historyApiFallback: true,
-        inline: false,
-        contentBase: __dirname + '/public',
-        port: NODE_PORT,
-        hot: true
-    },
-
-    plugins: [
-        // new webpack.ProvidePlugin({
-        //   $: 'jquery',
-        //   jQuery: 'jquery'
-        // }),
-        new ExtractTextPlugin('bundle.css', {
-            allChunks: true,
-            disable: process.env.NODE_ENV == 'development'
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ]
-};
-
-if (NODE_ENV == 'production') {
-    module.exports.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings:     false,
-                drop_console: true,
-                unsafe:       true
-            }
-        })
-    );
+  return sources;
 }
 
-function getEntrySources(sources) {
-    if (process.env.NODE_ENV !== 'production') {
-        sources.unshift('react-hot-loader/patch');
-        sources.unshift(`webpack-dev-server/client?http://${NODE_HOST}:${NODE_PORT}`);
-        sources.unshift('webpack/hot/only-dev-server');
-    }
 
-    return sources;
+module.exports = {
+  entry: {
+    js: getEntrySources(['./src/main.js']),
+    vendor: [
+      'react',
+      'react-dom',
+      'react-tap-event-plugin',
+      'redux',
+      'redux-thunk',
+      'react-redux'
+    ]
+  },
+  output: {
+    path: path.join(__dirname, '/public/build/'),
+    publicPath: '/build/',
+    filename: 'bundle.js',
+    chunkFilename: '[name].bundle.js'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  performance: {
+    hints: isProd ? 'warning' : false
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        enforce: 'pre',
+        use: ['eslint-loader'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        use: isProd
+              ? ExtractTextPlugin.extract({ fallback: 'style-loader', loader: 'css-loader' })
+              : ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: isProd
+              ? ExtractTextPlugin.extract({ fallback: 'style-loader',
+                loader: ['css-loader', 'less-loader'] })
+              : ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png)$/,
+        use: [
+          'file-loader?name=[name].[ext]',
+          {
+            loader: 'img-loader',
+            query: {
+              gifsicle: { interlaced: false },
+              jpegtran: {
+                progressive: true,
+                arithmetic: false
+              },
+              optipng: { optimizationLevel: 5 },
+              pngquant: {
+                floyd: 0.5,
+                speed: 2
+              },
+              svgo: {
+                plugins: [
+                  { removeTitle: true },
+                  { convertPathData: false }
+                ]
+              }
+            }
+          }
+        ],
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: 'file-loader?name=[name].[ext]',
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['es2015', { modules: false }],
+                'react',
+                'stage-2'
+              ]
+            }
+          }
+        ],
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    inline: false,
+    contentBase: path.join(__dirname, '/public'),
+    port: NODE_PORT,
+    hot: true,
+    stats: {
+      assets: true,
+      children: false,
+      chunks: false,
+      hash: false,
+      modules: false,
+      publicPath: false,
+      progress: true,
+      timings: true,
+      version: false,
+      warnings: true,
+    }
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.bundle.js'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+    })
+  ]
+};
+
+if (isProd) {
+  const productionPlugins = [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+      postcss: [
+        mixins(),
+        autoprefixer({
+          browsers: 'last 10 versions'
+        })
+      ],
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false
+      },
+    }),
+    new ExtractTextPlugin({
+      filename: 'bundle.css',
+      allChunks: true
+    })
+  ];
+
+  module.exports.plugins.push(...productionPlugins);
+} else {
+  const productionPlugins = [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ];
+
+  module.exports.plugins.push(...productionPlugins);
 }

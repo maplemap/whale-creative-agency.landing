948555fb67f920c34e07bb069a6584e449d383dd
Sergey.Illarionov <easthunch@gmail.com>
2017-03-09 13:12:00 +0300

    refactoring: guidlines

--- src/components/GuidLines.jsx
+++ src/components/GuidLines.jsx
@@ -1,37 +1,43 @@
-import React from "react";
-import './GuidLines.less';
-
+import React from 'react';
 import classNames from 'classnames';
 
+import getUniqueID from '../utils/getUniqueID';
+
+import './GuidLines.less';
+
 class GuidLines extends React.Component {
-    static defaultProps = {
-        lines: [1, 2, 3, 4, 5, 6]
+  static defaultProps = {
+    lines: [1, 2, 3, 4, 5, 6]
+  };
+
+  render() {
+    const disableLines = this.props.disableLines;
+
+    const guidClassConditions = {
+      'guid-lines--white': (this.props.colorScheme === 'white')
     };
+    const guidClass = classNames('guid-lines', guidClassConditions);
 
-    render() {
-        const disableLines = this.props.disableLines;
+    return (
+      <div className={guidClass} >
+        {
+          this.props.lines.map((num) => {
+            const spanClassConditions = {
+              hidden: (disableLines && disableLines.indexOf(num) + 1)
+            };
+            const spanClass = classNames(`guid-lines__${num}`, spanClassConditions);
 
-        const guidClassConditions = {
-            "guid-lines--white": (this.props.colorScheme === 'white')
-        };
-        const guidClass = classNames("guid-lines", guidClassConditions);
-
-        return (
-            <div className={ guidClass }>
-                { this.props.lines.map((num, index) => {
-                    const spanClassConditions = {
-                      hidden: ( disableLines && disableLines.indexOf(num) + 1 )
-                    };
-                    const spanClass = classNames(`guid-lines__${num}`, spanClassConditions);
-
-                    return <span
-                                key={index}
-                                className={spanClass}
-                           />
-                })}
-            </div>
-        )
-    }
+            return (
+              <span
+                key={getUniqueID()}
+                className={spanClass}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default GuidLines;

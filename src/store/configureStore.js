e9bd3c704e45adda0174b1c23f66a21bbba52d07
Sergey.Illarionov <easthunch@gmail.com>
2017-03-22 17:09:00 +0300

    add: redux-thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,11 @@
-import { createStore } from 'redux';
-import rootReducer from '../reducers/index';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from '../reducers';
 
-export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+export default function configureStore(initialState = {}) {
+  const store = compose(
+    applyMiddleware(thunkMiddleware)
+  )(createStore)(rootReducer, initialState);
 
   return store;
 }

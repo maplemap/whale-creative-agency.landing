2bcafcfcb2b138c0fbf52b47e1945e500df3d36b
Sergey.Illarionov <easthunch@gmail.com>
2017-04-05 17:21:00 +0300

    refactoring: teamslide 3

--- src/components/Team/TeamSlide_3.jsx
+++ src/components/Team/TeamSlide_3.jsx
@@ -1,25 +1,22 @@
-import React from "react";
+import React from 'react';
 
-class Slide_3 extends React.Component {
+const Slide3 = () => (
+  <div className="team__slide team__slide--3">
+    <div className="team__body">
+      <article className="description description--team" data-num="3">
+        <h2 className="description__title">About us</h2>
+        <hr className="description__stroke" />
+        <p className="description__text">
+            We are a small family of passionate and creative people. Located in Cherkasy Ukraine.
+            Our goal is to drive your business growth by keeping design affordable
+             yet still beautiful and exclusive.
+            We offer a full stack of design services: brand identity, logo design,
+             mobile app and website design,
+            marketing materials for print and even music production.
+        </p>
+      </article>
+    </div>
+  </div>
+);
 
-    render() {
-        return(
-            <div className="team__slide team__slide--3">
-                <div className="team__body">
-                    <article className="description description--team" data-num="3">
-                        <h2 className="description__title">About us</h2>
-                        <hr className="description__stroke" />
-                        <p className="description__text">
-                            We are a small family of passionate and creative people. Located in Cherkasy Ukraine.
-                            Our goal is to drive your business growth by keeping design affordable yet still beautiful and exclusive.
-                            We offer a full stack of design services: brand identity, logo design, mobile app and website design,
-                            marketing materials for print and even music production.
-                        </p>
-                    </article>
-                </div>
-            </div>
-        )
-    }
-}
-
-export default Slide_3;
+export default Slide3;

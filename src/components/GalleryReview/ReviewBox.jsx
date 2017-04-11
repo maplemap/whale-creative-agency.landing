8bd41a80780ee096dc3eb07b85c731fd9a38be9b
Sergey.Illarionov <easthunch@gmail.com>
2017-04-11 14:43:00 +0300

    refactoring: ReviewBox

--- src/components/GalleryReview/ReviewBox.jsx
+++ src/components/GalleryReview/ReviewBox.jsx
@@ -1,59 +1,63 @@
-import React from "react";
-import ImageLoading from '../../utils/ImageLoading.jsx';
+import React from 'react';
+import ImageLoading from '../ImageLoading';
 
+import getUniqueID from '../../helpers/getUniqueID';
 
 class ReviewBox extends React.Component {
-    state = {
-        imageViewFilename: this.props.project.images[0],
-        activePreviewIndex: 0
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageViewFilename: this.props.project.images[0],
+      activePreviewIndex: 0
     };
+  }
 
-    handlerClickItemPreviewImg = (imageName, index) => {
-        this.setState({
-            imageViewFilename: imageName,
-            activePreviewIndex: index
-        });
-    };
+  handlerClickItemPreviewImg = (imageName, index) => {
+    this.setState({
+      imageViewFilename: imageName,
+      activePreviewIndex: index
+    });
+  };
 
-    render() {
-        const that = this;
-
-        return(
-            <div className="gallery-review__body">
-                <span
-                    className="close-btn close-btn--gallery-review"
-                    onClick={this.props.onClickCloseReview}
-                />
-                <div className="gallery-review__project-name"> {this.props.project.name} </div>
-                <div className="gallery-review__project-customer"> {this.props.project.customer} </div>
-                <div className="gallery-review__project-view">
-                    <ImageLoading
-                        src={`static/projects/${this.state.imageViewFilename}`}
-                        alt={this.props.project.name}
-                    />
-                </div>
-                <div className="gallery-review__project-preview">
-                    {
-                        this.props.project.images.map((imageName, index) => {
-                            if (index < this.props.amountImgPreview) {
-                                return <div
-                                    key={index}
-                                    className={`gallery-review__project-preview-item ${(index === this.state.activePreviewIndex) ? 'gallery-review__project-preview-item--active' : ''}`}
-                                    onClick={that.handlerClickItemPreviewImg.bind(null, imageName, index)}
-                                >
-                                    <ImageLoading
-                                        className="gallery-review__project-image"
-                                        src={`static/projects/${imageName}`}
-                                        alt={that.props.project.name}
-                                    />
-                                </div>
-                            }
-                        })
-                    }
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="gallery-review__body">
+        <button
+          className="close-btn close-btn--gallery-review"
+          onClick={this.props.onClickCloseReview}
+        />
+        <div className="gallery-review__project-name"> {this.props.project.name} </div>
+        <div className="gallery-review__project-customer"> {this.props.project.customer} </div>
+        <div className="gallery-review__project-view">
+          <ImageLoading
+            src={`static/projects/${this.state.imageViewFilename}`}
+            alt={this.props.project.name}
+          />
+        </div>
+        <div className="gallery-review__project-preview">
+          {
+            this.props.project.images.map((imageName, index) => (
+              (index < this.props.amountImgPreview)
+              ?
+                <button
+                  key={getUniqueID()}
+                  className={`gallery-review__project-preview-item ${(index === this.state.activePreviewIndex) ? 'gallery-review__project-preview-item--active' : ''}`}
+                  onClick={() => this.handlerClickItemPreviewImg(imageName, index)}
+                >
+                  <ImageLoading
+                    className="gallery-review__project-image"
+                    src={`static/projects/${imageName}`}
+                    alt={this.props.project.name}
+                  />
+                </button>
+              : null
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ReviewBox;

51ec028ae865eb7726296a5796c98e154ecbf485
Sergey.Illarionov <easthunch@gmail.com>
2017-04-17 14:01:00 +0300

    refactoring: ProtfolioGallery

--- src/components/Portfolio/PortfolioGallery.jsx
+++ src/components/Portfolio/PortfolioGallery.jsx
@@ -1,63 +1,64 @@
-import React from "react";
-
+import React from 'react';
 import Masonry from 'react-masonry-component';
 
 
 class PortfolioGallery extends React.Component {
-
-    static defaultProps = {
-        masonryOptions: {
-            gutter: 20
-        }
-    };
-
-    componentWillReceiveProps(nextProps){
-        if (JSON.stringify(nextProps.projects) !== JSON.stringify(this.props.projects)) {
-            this._appearEffectForGrid()
-        }
+  static defaultProps = {
+    masonryOptions: {
+      gutter: 20
     }
+  };
 
-    render() {
-        const projects = this.props.projects;
-
-        return (
-            <div className="gallery gallery--portfolio">
-                <div ref="grid" className="gallery__grid">
-                    <Masonry options={this.props.masonryOptions}>
-                        {
-                            projects.map(project =>
-                                <div
-                                    key={project.id}
-                                    data-index={project.id}
-                                    className="gallery__item"
-                                    onClick={this.props.onClickGalleryItem}
-                                >
-                                    <img src={`static/projects/${project.shortcut}`} alt={project.name} />
-                                    <div className="gallery__mask">
-                                        <div
-                                           className="gallery__see-icon"
-                                           title={`see ${project.name}`}
-                                        >
-                                            See project
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        }
-                    </Masonry>
-                </div>
-            </div>
-        )
+  componentWillReceiveProps(nextProps) {
+    if (JSON.stringify(nextProps.projects) !== JSON.stringify(this.props.projects)) {
+      this.appearEffectForGrid();
     }
+  }
 
-    _appearEffectForGrid = () => {
-        const gridClasses = this.refs.grid.classList;
-        gridClasses.add("animated", "fadeInUp");
+  appearEffectForGrid = () => {
+    const galleryGridClassList = this.galleryGrid.classList;
+    galleryGridClassList.add('animated', 'fadeInUp');
 
-        setTimeout(() => {
-            gridClasses.remove("animated", "fadeInUp");
-        }, 1000);
-    }
+    setTimeout(() => {
+      galleryGridClassList.remove('animated', 'fadeInUp');
+    }, 1000);
+  }
+
+  render() {
+    const projects = this.props.projects;
+
+    return (
+      <div className="gallery gallery--portfolio">
+        <div
+          ref={c => (this.galleryGrid = c)}
+          className="gallery__grid"
+        >
+          <Masonry options={this.props.masonryOptions}>
+            {
+              projects.map(project =>
+                <button
+                  key={project.id}
+                  data-index={project.id}
+                  className="gallery__item"
+                  onClick={this.props.onClickGalleryItem}
+                >
+                  <img src={`static/projects/${project.shortcut}`} alt={project.name} />
+                  <div className="gallery__mask">
+                    <div
+                      className="gallery__see-icon"
+                      title={`see ${project.name}`}
+                    >
+                        See project
+                    </div>
+                  </div>
+                </button>
+                )
+            }
+          </Masonry>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PortfolioGallery;

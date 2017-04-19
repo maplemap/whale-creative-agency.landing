8f199daf606ca5485d1124886c788e46a68bcf0e
Sergey.Illarionov <easthunch@gmail.com>
2017-04-19 16:49:00 +0300

    refactoring: PortfolioDescription

--- src/components/Portfolio/PortolioDescription.jsx
+++ src/components/Portfolio/PortolioDescription.jsx
@@ -1,54 +1,55 @@
-import React from "react";
+import React from 'react';
+import getUniqueID from '../../helpers/getUniqueID';
 
 class PortolioDescription extends React.Component {
-    render() {
-        const categories = this._getUniqueCategories();
+  getUniqueCategories() {
+    return [...new Set(this.props.projects.map(project => project.category))];
+  }
 
-        return(
-            <article className="description description--portfolio">
-                <h2 className="description__title">Our work</h2>
-                <ul className="gallery-menu gallery-menu--portfolio">
-                    <li
-                        className={`gallery-menu__item ${ (this.props.filter === 'all')? 'gallery-menu__item--current' : '' }`}
-                        data-category="all"
-                        onClick={this.props.onClickSortPortfolio}
-                    >
-                        All
-                    </li>
-                    {
-                        categories.map((category, index) =>
-                            <li
-                                key={index}
-                                data-category={category}
-                                className={`gallery-menu__item ${ (this.props.filter === category)? 'gallery-menu__item--current' : '' }`}
-                                onClick={this.props.onClickSortPortfolio}
-                            >
-                                {category}
-                            </li>
-                        )
-                    }
-                </ul>
-                <hr className="description__stroke" />
-                <p className="description__text">
-                    {this._getDescription( this.props.filter )}
-                </p>
-            </article>
-        )
+  getDescription = (category) => {
+    let description = this.props.description.default;
+
+    if (category !== 'all') {
+      description = this.props.description.galleryCategory[category];
     }
 
-    _getUniqueCategories() {
-        return [...new Set(this.props.projects.map(project => project.category))];
-    }
+    return description;
+  };
 
-    _getDescription = (category) => {
-        let description = this.props.description.default;
+  render() {
+    const categories = this.getUniqueCategories();
 
-        if(category !== 'all') {
-            description = this.props.description.galleryCategory[category];
-        }
-
-        return description;
-    };
+    return (
+      <article className="description description--portfolio">
+        <h2 className="description__title">Our work</h2>
+        <ul className="gallery-menu gallery-menu--portfolio">
+          <li
+            className={`gallery-menu__item ${(this.props.filter === 'all') ? 'gallery-menu__item--current' : ''}`}
+            data-category="all"
+            onClick={this.props.onClickSortPortfolio}
+          >
+            All
+          </li>
+          {
+            categories.map(category =>
+              <li
+                key={getUniqueID()}
+                data-category={category}
+                className={`gallery-menu__item ${(this.props.filter === category) ? 'gallery-menu__item--current' : ''}`}
+                onClick={this.props.onClickSortPortfolio}
+              >
+                {category}
+              </li>
+            )
+          }
+        </ul>
+        <hr className="description__stroke" />
+        <p className="description__text">
+          {this.getUniqueCategories(this.props.filter)}
+        </p>
+      </article>
+    );
+  }
 }
 
 export default PortolioDescription;

c4967703b347f754e5a375f39868dfb6234f9f48
Sergey.Illarionov <easthunch@gmail.com>
2017-04-24 11:49:00 +0300

    refactoring: Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,33 +1,35 @@
-import React from "react";
+import React from 'react';
 import classNames from 'classnames';
+import getUniqueID from '../helpers/getUniqueID';
+
 import './Navigation.less';
 
-class Navigation extends React.Component{
-    static defaultProps = {
-        spanAmount: [1, 2, 3, 4]
-    }
+class Navigation extends React.Component {
+  static defaultProps = {
+    spanAmount: [1, 2, 3, 4]
+  }
 
-    render() {
-        const navIconColorModifier = (this.props.colorScheme === 'white');
-        const navIconClassNames = classNames('navigation__icon', {
-            'navigation__icon--white': navIconColorModifier
-        }, {
-            'navigation__icon--open': this.props.isNavigationMenuActive
-        })
+  render() {
+    const navIconColorModifier = (this.props.colorScheme === 'white');
+    const navIconClassNames = classNames('navigation__icon', {
+      'navigation__icon--white': navIconColorModifier
+    }, {
+      'navigation__icon--open': this.props.isNavigationMenuActive
+    });
 
-        return (
-            <nav className="navigation">
-                <div
-                    className={navIconClassNames}
-                    onClick={this.props.handlerShowMenuNavigation}
-                >
-                    { this.props.spanAmount.map((num, index) => {
-                        return <span key={index} />
-                    })}
-                </div>
-            </nav>
-        )
-    }
+    return (
+      <nav className="navigation">
+        <button
+          className={navIconClassNames}
+          onClick={this.props.handlerShowMenuNavigation}
+        >
+          { this.props.spanAmount.map(() =>
+            <span key={getUniqueID()} />
+          )}
+        </button>
+      </nav>
+    );
+  }
 }
 
 export default Navigation;

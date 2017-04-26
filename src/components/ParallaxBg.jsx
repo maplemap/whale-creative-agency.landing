4657f5bc7abf37bd13142ac016c362ae1c067a57
Sergey.Illarionov <easthunch@gmail.com>
2017-04-26 16:21:13 +0300

    refactoring: ParallaxBg

--- src/components/ParallaxBg.jsx
+++ src/components/ParallaxBg.jsx
@@ -1,57 +1,61 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+// TODO: Refactoring
 
 class ParallaxBackground extends React.Component {
-    static defaultProps = {
-        disableParallax: false,
-        parallaxSpeed: 0.03
+  static defaultProps = {
+    disableParallax: false,
+    parallaxSpeed: 0.03
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      positionX: '',
+      positionY: '',
+      style: {}
     };
+  }
 
-    state = {
-        positionX: '',
-        positionY: '',
-        style: {}
-    };
+  componentDidMount() {
+    if (!this.props.disableParallax) {
+      window.addEventListener('scroll', this.handleScroll);
 
-    componentDidMount() {
-        if(this.props.disableParallax) return;
+      const parallaxBox = ReactDOM.findDOMNode( this.refs.parallaxBox );
+      const bgPosition = window.getComputedStyle(parallaxBox).getPropertyValue("background-position").split(' ');
 
-        window.addEventListener('scroll', this.handleScroll);
-
-        const parallaxBox = ReactDOM.findDOMNode( this.refs.parallaxBox );
-        const bgPosition = window.getComputedStyle(parallaxBox).getPropertyValue("background-position").split(' ');
-
-        this.setState({
-            positionX: parseInt(bgPosition[0], 10),
-            positionY: parseInt(bgPosition[1], 10)
-        });
+      this.setState({
+        positionX: parseInt(bgPosition[0], 10),
+        positionY: parseInt(bgPosition[1], 10)
+      });
     }
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll);
-    }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 
-    handleScroll = () => {
-        const newPositionY = this.state.positionY - window.pageYOffset * this.props.parallaxSpeed;
+  handleScroll = () => {
+    const newPositionY = this.state.positionY - window.pageYOffset * this.props.parallaxSpeed;
 
-        this.setState({
-            style: {
-                backgroundPosition: `${this.state.positionX}% ${newPositionY}%`
-            }
-        });
-    };
+    this.setState({
+      style: {
+        backgroundPosition: `${this.state.positionX}% ${newPositionY}%`
+      }
+    });
+  };
 
-    render() {
-        return(
-            <div
-                ref="parallaxBox"
-                className={this.props.className}
-                style={this.state.style}
-            >
-                {this.props.children}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div
+        ref={c => (this.parallaxBox = c)}
+        className={this.props.className}
+        style={this.state.style}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
 }
 
 export default ParallaxBackground;
